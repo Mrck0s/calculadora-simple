37d5dfc35b3a06bc005017205796335f87c15c5b
Mrck0s <110942627+Mrck0s@users.noreply.github.com>
2023-03-14 09:19:04 -0300

    Finish
    codigo terminado

--- js/script.js
+++ js/script.js
@@ -1,13 +1,48 @@
-const Screen = document.querySelector(".screen");
-const Btns = document.querySelectorAll(".btn");
+const numeros = document.querySelectorAll(".num");
+const operaciones = document.querySelectorAll(".op");
+const equal = document.querySelector(".eq");
+const ac = document.querySelector(".ac");
+const screen = document.querySelector(".screen");
+let primerNum = "";
+let segundoNum = "";
+let operacion = null;
 
-Btns.forEach(btn => {
-    btn.addEventListener("click", () => {
-        const btnClick = btn.textContent;
-        if (Screen.textContent === "Press a Button") {
-            Screen.textContent = btnClick;
+numeros.forEach((numero) =>  {
+    numero.addEventListener("click", () => {
+        if (operacion === null) {
+            primerNum += numero.innerText;
+            screen.innerText = primerNum;
         } else {
-            Screen.textContent += btnClick;
+            segundoNum += numero.innerText;
+            screen.innerText = segundoNum;
         }
     })
+})
+
+operaciones.forEach((op) => {
+    op.addEventListener("click", () => {
+        if (primerNum !== "" && segundoNum !== "") {
+            const resultado = eval(primerNum + operacion + segundoNum);
+            screen.innerText = resultado;
+            primerNum = resultado.toString();
+            segundoNum = "";
+            operacion = op.innerText;
+        } else {
+            operacion = op.innerText;
+        }
+    })
+})
+
+equal.addEventListener("click", () => {
+    const resultado = eval(primerNum + operacion + segundoNum);
+    screen.innerText = resultado;
+    primerNum = resultado.toString();
+    segundoNum = "";
+    operacion = null;
+})
+ac.addEventListener("click", () => {
+    screen.innerText = "0";
+    primerNum = "";
+    segundoNum = "";
+    operacion = null;
 })
